6199a614802fde52c70c39c8c967ac265fc58dac
Dong <70738721+FOaker@users.noreply.github.com>
2021-11-13 20:21:58 +0800

    update template

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+# Blog post data model
+class ArticlePost(models.Model):
+    # Article author
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Article title
+    title = models.CharField(max_length=100)
+
+    # Article body
+    body = models.TextField()
+
+    # Article creation time
+    created = models.DateTimeField(default=timezone.now)
+
+    # Article update time
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # reverse order
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
