2418952ed2ef921ad96a61cd03a5bb8ff8d22179
Dong <70738721+FOaker@users.noreply.github.com>
2021-11-15 21:30:28 +0800

    Update post article and delete article

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,19 +1,48 @@
-from django.shortcuts import render
+# import markdown
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from article.models import ArticlePost
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 
 
 def article_list(request):
-    # 取出所有博客文章
     articles = ArticlePost.objects.all()
-    # 需要传递给模板（templates）的对象
     context = {'articles': articles}
-    # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
 
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+    # article.body = markdown.markdown(article.body,
+    #             extensions=[
+    #             #'markdown.extensions.extra',
+    #             #'markdown.extensions.codehilite',
+    #             ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
+
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("The content of the form is incorrect, please fill in again")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
+
+
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        article.delete()
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("Only allow post requests")
